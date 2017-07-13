62be4739e09e3b842c19971063a927bddfd6c9e3
loatheb <zz.innj@gmail.com>
2017-07-13 09:48:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import hypernova from 'hypernova';
-import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import root from 'window-or-global'
+import root from 'window-or-global';
 
 root.ReactDOMServer = ReactDOMServer;
 
-const renderString = (name, configureStore) => {
-  return hypernova({
-    server: () => props => configureStore.server(props),
-    client: () => {},
-  });
+function uuid() {
+  return (
+    [1e7] +
+    -1e3 +
+    -4e3 +
+    -8e3 +
+    -1e11
+  ).replace(
+    /[018]/g,
+    x => (x ^ Math.random() * 16 >> x / 4).toString(16),
+  );
 }
+
+function serialize(html) {
+  const id = uuid();
+  const markup = `<!-- hypernova-id: ${id} -->${html}`;
+  return `${markup}`;
+}
+
+const renderString = (name, configureStore) => hypernova({
+  server: () => props => serialize(configureStore.server(props)),
+  client: () => {},
+});
 
 export default renderString.renderString = renderString.default = renderString;
